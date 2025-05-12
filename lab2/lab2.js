5f65895995c65a0f85077f40f1e7038345078258
cini4ka1 <kkrakov71@gmail.com>
2025-05-12 17:04:43 +0300

    1

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,8 +1,8 @@
 /**
- * 
- * @param {number} x 
- * @param {number} n 
- * @returns {number} 
+ * Возвращает x в степени n (n - целое число).
+ * @param {number} x - Основание степени.
+ * @param {number} n - Показатель степени (целое число).
+ * @returns {number} Результат возведения x в степень n.
  */
 function pow(x, n) {
     if (n === 0) return 1;
@@ -16,27 +16,27 @@ function pow(x, n) {
 }
 
 /**
- *
- * @param {number} n 
- * @returns {number} 
+ * Вычисляет сумму чисел от 1 до n включительно.
+ * @param {number} n - Натуральное число.
+ * @returns {number} Сумма чисел от 1 до n.
  */
 function sumTo(n) {
     return n * (n + 1) / 2;
 }
 
 /**
- * 
- * @param {number} year 
- * @returns {boolean} 
+ * Проверяет год на високосность.
+ * @param {number} year - Год для проверки.
+ * @returns {boolean} true, если год високосный, иначе false.
  */
 function isLeapYear(year) {
     return (year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0);
 }
 
 /**
- * 
- * @param {number} n - 
- * @returns {bigint} 
+ * Возвращает факториал числа n (n!).
+ * @param {number} n - Число для вычисления факториала.
+ * @returns {bigint} Факториал числа n в виде BigInt.
  */
 function factorial(n) {
     if (n === 0) return 1n;
@@ -44,9 +44,9 @@ function factorial(n) {
 }
 
 /**
- * 
- * @param {number} n 
- * @returns {bigint} n
+ * Возвращает n-е число Фибоначчи.
+ * @param {number} n - Порядковый номер числа Фибоначчи.
+ * @returns {bigint} n-е число Фибоначчи в виде BigInt.
  */
 function fib(n) {
     let a = 0n;
@@ -62,9 +62,9 @@ function fib(n) {
 }
 
 /**
- * 
- * @param {number} x 
- * @returns {function} 
+ * Возвращает функцию, которая сравнивает свой аргумент с x.
+ * @param {number} x - Число для сравнения.
+ * @returns {function} Функция, которая принимает y и сравнивает его с x.
  */
 function compare(x) {
     return function(y) {
@@ -75,18 +75,18 @@ function compare(x) {
 }
 
 /**
- *
- * @param {...number} args 
- * @returns {number}
+ * Возвращает сумму всех переданных аргументов.
+ * @param {...number} args - Числа для суммирования.
+ * @returns {number} Сумма всех аргументов.
  */
 function sum(...args) {
     return args.reduce((total, current) => total + current, 0);
 }
 
 /**
- * 
- * @param {Object} obj 
- * @returns {Object}
+ * Добавляет символьное свойство blackSpot со значением true к переданному объекту.
+ * @param {Object} obj - Объект для модификации.
+ * @returns {Object} Исходный объект с добавленным свойством blackSpot.
  */
 function addBlackSpot(obj) {
     obj[Symbol.for("blackSpot")] = true;
